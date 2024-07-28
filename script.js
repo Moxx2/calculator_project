6f29da1353eb0dc9dd24e730d2e4071d5495f95a
Moxx2 <moxxlearnin@Xubuntu.myguest.virtualbox.org>
2024-07-28 11:59:37 +0300

    Added interface
    I created basic functions (add, subtract, etc) and variables, created buttons, display and styled them.

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+let firstNum = '';
+let operator = '';
+let secondNum = '';
+
+function operate(firstNum, operator, secondNum) {
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        break;
+        case '-':
+            return subtract(firstNum, secondNum);
+        break;
+        case '*':
+            return multiply(firstNum, secondNum);
+        break;
+        case '/':
+            return divide(firstNum, secondNum);
+        break;
+    };
+};
